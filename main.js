71c15bae0ae4eb4d7cf9e6e03934b2be4d67e4c3
justspr <142838185+justspr@users.noreply.github.com>
2024-09-23 11:57:36 -0300

    kk

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 let nomeusuario="Júlia";
 // alert (nomeusuario)
 function soma (num1, num2){
-return num1+num2
-}
+return num1+num2}
 console.log(soma(4,2));
+
+function areaQuadrado (lado){
+    return lado**2;
+}
+console.log (areaQuadrado)
+
+let areaTriangulo =
+function (base, altura) {
+    return (base*altura)/2;
+}
+console.log (areaTriangulo)
